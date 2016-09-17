f2fb344447d21e984357d179cae10da95e5d9172
Matthew Thompson <matt@mattathompson.org>
2016-09-17 15:35:20 -0400

    Adds a GameBoard component to the project as a child component to the AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,13 +21,13 @@ describe('App: ConnectFour', () => {
   it(`should have as title 'app works!'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Ng2 Connect Four');
   }));
 
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Ng2 Connect Four');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { GameBoard } from './game-board/game-board.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  directives: [ GameBoard ],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
