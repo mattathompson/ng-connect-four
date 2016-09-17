dd8a9c1081d485d895732e68ec30173eb447df1c
Matthew Thompson <matt@mattathompson.org>
2016-09-17 17:59:30 -0400

    Creates a board-space component

---
+++ src/app/board-space/board-space.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { BoardSpaceComponent } from './board-space.component';
+
+describe('Component: BoardSpace', () => {
+  it('should create an instance', () => {
+    let component = new BoardSpaceComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/board-space/board-space.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'td[app-board-space]',
+  templateUrl: './board-space.component.html',
+  styleUrls: ['./board-space.component.css']
+})
+export class BoardSpaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
