1f4aaf1f35cd017eaaacb969defb94a934f7a117
Matthew Thompson <matt@mattathompson.org>
2016-09-17 18:00:04 -0400

    Adds the board-row component to the module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { GameBoardComponent } from './game-board/game-board.component';
 import { BoardRowComponent } from './board-row/board-row.component';
+import { BoardSpaceComponent } from './board-space/board-space.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameBoardComponent,
-    BoardRowComponent
+    BoardRowComponent,
+    BoardSpaceComponent
   ],
   imports: [
     BrowserModule,
